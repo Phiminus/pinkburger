63be0b2cfae1a8f25cdd6d6fdacc85a92c95a0e0
phiminus <Neets@live.co.kr>
2017-06-03 14:08:35 +0900

    커밋

---
+++ pinkburger/app/src/main/java/xyz/c1oud/pinkburger/MainActivity.java
@@ -0,0 +1,13 @@
+package xyz.c1oud.pinkburger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
